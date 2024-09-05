296983d401abf0edb41c7f1747c40b6edfee628c
Michael Bain <150988106+Mikebain2387@users.noreply.github.com>
2024-09-05 14:00:15 -0500

    update

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let taskItem = document.getElementById('list');
 
 submit.addEventListener('click', addNewItem)
 
-function addNewItem(){
+function addNewItem(text){
  if(addTask.value != '') //checks to see if addTask input is not equal to an empty string
  {
   let listItem = document.createElement("li"); // creating a new li element
@@ -17,51 +17,24 @@ listItem.style.color = 'ivory';
  listItem.style.background = 'cadetblue';
  listItem.style.marginBottom = '10px';
  newTask.style.padding = '0px, 10px, 0px, 0px';
-  
+ /*
+ listItem.innerHTML = 
+ 
+ '<button id="remove" class="remove"></button>';
+  */
   document.getElementById("theList").appendChild(listItem); // sends li into parent element ul with id= "theList"
-/*
-checkbox needs work
-  let checkbox = document.createElement('input'); // creates  new checkbox element
-  checkbox.type = 'checkbox'; //sets input to checkbox
 
-  listItem.appendChild(checkbox); //sends new checkbox item to parent element li
   
-*/
-  
-
  clearInput() //calls function to set input to an empty string after everything else has ran
  }
+
+ 
  
 }
-function complete(){
 
-}
 
 function clearInput(){
  newTask.value = ""; //sets input to an empty string which brings back default placeholder "new task"
 }
-/*
-how to delete checked item???
-
-deleteItem.addEventListener("click", removeItem)
-function removeItem(){
- if(checkbox.checked){
-  listItem.value = "";
- }
-}
-
-*/
-
-/*
-trying to add checkbox may not be bext choice
-
-function addCheckBox(){
- if( != ""){
-  let checkItem = document.createElement("input");
-  checkItem.type = 'checkbox';
-  document.getElementsByTagName("li").appendChild(checkItem);
- }
-}
-
-*/
+ 
 
