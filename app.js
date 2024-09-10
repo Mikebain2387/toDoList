3ae3552615954d69a542f54ec759d54319d2032e
Michael Bain <150988106+Mikebain2387@users.noreply.github.com>
2024-09-10 11:28:26 -0500

    update

--- app.js
+++ app.js
@@ -1,40 +1,26 @@
-let newTask = document.getElementById('addTask');
-let submit = document.getElementById('addBtn');
-let deleteItem = document.getElementById('remove');
-let taskItem = document.getElementById('list');
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
 
-// take value from input and send it to the list when the add button is clicked
+todoButton.addEventListener("click", addTodo);
 
-submit.addEventListener('click', addNewItem)
+function addTodo(event){
+  event.preventDefault();
+// new div 
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
 
-function addNewItem(text){
- if(addTask.value != '') //checks to see if addTask input is not equal to an empty string
- {
-  let listItem = document.createElement("li"); // creating a new li element
-  listItem.innerText = addTask.value; //giving new list item value from input
-  listItem.classList.add = ("newLi"); //assigning list item class name for styling purposes
-listItem.style.color = 'ivory';
- listItem.style.background = 'cadetblue';
- listItem.style.marginBottom = '10px';
- newTask.style.padding = '0px, 10px, 0px, 0px';
- /*
- listItem.innerHTML = 
- 
- '<button id="remove" class="remove"></button>';
-  */
-  document.getElementById("theList").appendChild(listItem); // sends li into parent element ul with id= "theList"
+  //create new li
+  const newTodo = document.createElement('li');
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add('todo-item');
+  todoDiv.appendChild(newTodo);
 
-  
- clearInput() //calls function to set input to an empty string after everything else has ran
- }
+  //checkmark
+const completedButton = document.createElement('button')
+completedButton.classList.add('complete-btn');
+todoDiv.appendChild(completedButton);
 
- 
- 
+  //delete button
+
 }
-
-
-function clearInput(){
- newTask.value = ""; //sets input to an empty string which brings back default placeholder "new task"
-}
- 
-
