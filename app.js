f7bd4055c94c8143bbd4863a74d4e586c0396621
Michael Bain <150988106+Mikebain2387@users.noreply.github.com>
2024-09-10 11:49:56 -0500

    cleaning up code

--- app.js
+++ app.js
@@ -2,25 +2,34 @@ const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 
-todoButton.addEventListener("click", addTodo);
 
+
+//event listeners
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteTask);
+todoList.addEventListener("click", completeTask);
+
+
+// function to add new task
 function addTodo(event){
   event.preventDefault();
-// new div 
-  const todoDiv = document.createElement("div");
-  todoDiv.classList.add("todo");
+  if(todoInput.value !==""){
 
-  //create new li
-  const newTodo = document.createElement('li');
-  newTodo.innerText = todoInput.value;
-  newTodo.classList.add('todo-item');
-  todoDiv.appendChild(newTodo);
-
-  //checkmark
-const completedButton = document.createElement('button')
-completedButton.classList.add('complete-btn');
-todoDiv.appendChild(completedButton);
-
-  //delete button
-
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    
+    //create new li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    
+    //checkmark
+    const completedButton = document.createElement('button')
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+    
+    //delete button
+    
+  }
 }
